83ebb6bc5c06dfebbf01323e873557c2d173ab07
cdondrup <cdondrup@lincoln.ac.uk>
2013-07-24 15:46:34 +0100

    adding safety measures

--- scitos_ptu_mirror_head/src/scripts/ptu_mirror.py
+++ scitos_ptu_mirror_head/src/scripts/ptu_mirror.py
@@ -7,7 +7,11 @@ import math
 
 class HeadTeleop():
 	"A class to command the PTU to miror the movements of the Scitos head"
-
+	MAX_PAN=80
+	MIN_PAN=-80
+	MAX_TILT=10
+	MIN_TILT=-10
+	
 	def __init__(self):
 		rospy.init_node('ptu_mirror')
 		self.pub = rospy.Publisher('/ptu/cmd', JointState)
@@ -24,11 +28,14 @@ class HeadTeleop():
 	def callback(self, head_state):
 		self.ptu_command.position = []
 		for index, item in enumerate(head_state.name):
-			print index
 			if item == "HeadPan":
-				self.ptu_command.position.append(head_state.position[index]*(math.pi/180))
+				pan = head_state.position[index] if head_state.position[index] < MAX_PAN else MAX_PAN
+				pan = head_state.position[index] if head_state.position[index] > MIN_PAN else MIN_PAN
+				self.ptu_command.position.append(pan*(math.pi/180))
 			if item == "HeadTilt":
-				self.ptu_command.position.append(-head_state.position[index]*(math.pi/180))
+				tilt = head_state.position[index] if head_state.position[index] < MAX_TILT else MAX_TILT
+				tilt = head_state.position[index] if head_state.position[index] > MIN_TILT else MIN_TILT
+				self.ptu_command.position.append(-tilt*(math.pi/180))
 		self.pub.publish(self.ptu_command)
 
 if __name__ == '__main__':
