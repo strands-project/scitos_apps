96bf46ec5fc00b8fbcc4b7b93b94ef6bfc003256
cdondrup <cdondrup@lincoln.ac.uk>
2013-07-24 15:27:51 +0100

    stupid bug

--- scitos_ptu_mirror_head/src/scripts/ptu_mirror.py
+++ scitos_ptu_mirror_head/src/scripts/ptu_mirror.py
@@ -22,7 +22,7 @@ class HeadTeleop():
 		rospy.loginfo("Start")
 	
 	def callback(self, head_state):
-		self.ptu_command.position = [0, 0]
+		self.ptu_command.position = []
 		for index, item in enumerate(head_state.name):
 			print index
 			if item == "HeadPan":
