d0ccffc2248aecd995f50cf26046652c2cbef0e7
Rares Ambrus <raambrus@kth.se>
2014-05-13 21:23:11 +0200

    metric map action server now preempts flir action server instead of waiting for it to finish

--- scitos_ptu/scripts/ptu_action_server_metric_map.py
+++ scitos_ptu/scripts/ptu_action_server_metric_map.py
@@ -3,20 +3,22 @@
 import roslib; 
 import rospy
 import actionlib
+from actionlib_msgs.msg import GoalStatus
 
-from scitos_ptu.msg import *
+import scitos_ptu.msg
 from sensor_msgs.msg import *
 from std_msgs.msg import String
+from flir_pantilt_d46.msg import *
+import threading
 import math
 import time
-import flir_pantilt_d46.msg
 
 class PTUServer:
   def __init__(self):
     rospy.init_node('ptu_actionserver_metric_map')
     self.log_pub = rospy.Publisher('/ptu/log', String)
 
-    self.server = actionlib.SimpleActionServer('ptu_pan_tilt_metric_map', PanTiltAction, self.execute, False)
+    self.server = actionlib.SimpleActionServer('ptu_pan_tilt_metric_map', scitos_ptu.msg.PanTiltAction, self.execute, False)
     self.server.register_preempt_callback(self.preemptCallback)
     self.server.start()
     rospy.loginfo('Ptu action server started')
@@ -83,14 +85,14 @@ class PTUServer:
     self.preempted = False
     
     for i in range(panstart, panend, panstep):
-       if _get_preempt_status() or self.aborted:
+       if self._get_preempt_status() or self.aborted:
                 break;	 
        for j in range(tiltstart, tiltend, tiltstep):
-		if _get_preempt_status() or self.aborted:
+		if self._get_preempt_status() or self.aborted:
                 	break;	
     		self.ptugoal.pan = -i
 		self.ptugoal.tilt =-j
-    		_sendPTUGoal(self.ptugoal)
+    		self._sendPTUGoal(self.ptugoal)
 		# keep this position for logging
           	self.log_pub.publish("start_position")
 		time.sleep(2) # sleep for 2 seconds here
@@ -100,10 +102,10 @@ class PTUServer:
 
     self.ptugoal.pan = 0
     self.ptugoal.tilt =0
-    _sendPTUGoal(self.ptugoal)
+    self._sendPTUGoal(self.ptugoal)
     self.log_pub.publish("end_sweep")
     
-    if self.cancelled:
+    if self._get_preempt_status():
 		self.log_pub.publish("preempted")
 		self.result.success = False
 		self.server.set_preempted(self.result)
@@ -115,17 +117,17 @@ class PTUServer:
 		self.result.success = True
 		self.server.set_succeeded(self.result)
 
-  def _sendPTUGoal(ptu_goal):
+  def _sendPTUGoal(self, ptu_goal):
 	time_waited = 0
 	self.client.send_goal(self.ptugoal)
-	self.client.wait_for_result(self.preempt_timeout)
+	self.client.wait_for_result(rospy.Duration(self.preempt_timeout))
 	status= self.client.get_state()
-	while not status == GoalStatus.SUCCEEDED and not time_waited > self.pan_tilt_timeout and not _get_preempt_status():
+	while not status == GoalStatus.SUCCEEDED and not time_waited > self.pan_tilt_timeout and not self._get_preempt_status():
 		time_waited += self.preempt_timeout
-		self.client.wait_for_result(self.preempt_timeout)
+		self.client.wait_for_result(rospy.Duration(self.preempt_timeout))
 		status= self.client.get_state()
 
-	if _get_preempt_status():
+	if self._get_preempt_status():
 		# this action server has been preempted; preempt the other one as well
 		self.client.cancel_goal()
  	elif time_waited > self.pan_tilt_timeout or status != GoalStatus.SUCCEEDED:
@@ -133,9 +135,9 @@ class PTUServer:
 		self.client.cancel_goal()
 		self.aborted = True		
 
-   def _get_preempt_status(self):
+  def _get_preempt_status(self):
 	self.preempt_lock.acquire()
-	preempted = self.cancelled
+	preempted = self.preempted
 	self.preempt_lock.release()
 	return preempted	
 
