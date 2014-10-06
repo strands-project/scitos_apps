93b8c2c6639d05d1b77e51d28b74c40f8ae2c478
cburbridge <cburbridge@gmail.com>
2014-10-06 18:06:29 +0100

    PC name as parameter.

--- src/pc_monitor.py
+++ src/pc_monitor.py
@@ -5,8 +5,9 @@ import sysmon
 from diagnostic_msgs.msg import DiagnosticArray, DiagnosticStatus, KeyValue
 
 class PCMonitor(object):
-    def __init__(self, diagnostic_freq=0.2, wlan_iface="wlan0"):
+    def __init__(self, diagnostic_freq=0.2, pc_name="PC0", wlan_iface="wlan0"):
         self._diagnostic_freq = diagnostic_freq
+        self._pc_name = pc_name
         self._wlan_mon = sysmon.WirelessMonitor(iface=wlan_iface)
         self._cpu_temp_mon = sysmon.CPUTemperature()
         self._cpu_usage_mon = sysmon.CPUUsage()
@@ -36,7 +37,7 @@ class PCMonitor(object):
             for mon in self._monitors:
                 d = DiagnosticStatus()
                 d.name=mon.get_name()
-                d.hardware_id="PC0"
+                d.hardware_id = self._pc_name
                 d.message=mon.get_field_value("status")
                 d.level=mon.get_field_value("status_level")
                     
@@ -59,5 +60,6 @@ class PCMonitor(object):
 if __name__ == '__main__':
     rospy.init_node('pc_monitor')
     wlan_iface = rospy.get_param("wireless_iface", "wlan0")
-    monitor = PCMonitor(wlan_iface=wlan_iface)
+    pc_name = rospy.get_param("pc_name", "PC0")
+    monitor = PCMonitor(pc_name=pc_name, wlan_iface=wlan_iface)
     monitor.spin()
