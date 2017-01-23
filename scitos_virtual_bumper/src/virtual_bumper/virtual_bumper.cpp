04eb5ade2327205363e027ecaa74e9932975243a
Christian Dondrup <cdondrup@lincoln.ac.uk>
2017-01-23 23:04:21 +0000

    Publishing events

--- scitos_virtual_bumper/src/virtual_bumper/virtual_bumper.cpp
+++ scitos_virtual_bumper/src/virtual_bumper/virtual_bumper.cpp
@@ -5,15 +5,32 @@
 #include <nav_msgs/Odometry.h>
 #include <geometry_msgs/Twist.h>
 #include <geometry_msgs/PoseWithCovariance.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/Pose.h>
 
 static int maxSteady = 30;
-ros::Subscriber odomSub,cmdSub,motSub;
+ros::Subscriber odomSub,cmdSub,motSub,poseSub;
+geometry_msgs::PoseStamped currentPose;
+ros::Publisher bumperPose;
 ros::ServiceClient enableMotors; 
 bool steady = false;
 bool stopped = true;
 int steadyCnt = 0;
 
 bool freeRun = false;
+
+void reportPosition(bool pushed)
+{
+	geometry_msgs::PoseStamped pos;
+	pos.header.frame_id = "/map";
+	pos.header.stamp = ros::Time::now();
+	pos.pose.position.x = currentPose.pose.position.x;
+	pos.pose.position.y = currentPose.pose.position.y;
+	if (pushed == false) pos.pose.position.z = -1;
+	if (pushed == true) pos.pose.position.z = -1;
+	pos.pose.orientation = currentPose.pose.orientation; 
+	bumperPose.publish(pos);
+}
 
 void motorCallback(const scitos_msgs::MotorStatus &msg)
 {
@@ -23,6 +40,13 @@ void motorCallback(const scitos_msgs::MotorStatus &msg)
 void cmdCallback(const geometry_msgs::Twist &msg)
 {
 	if (msg.linear.x == 0 && msg.angular.z == 0) stopped = true; else stopped = false;
+}
+
+void poseCallback(const geometry_msgs::Pose::ConstPtr& msg)
+{
+	currentPose.pose.position.x = msg->position.x;
+	currentPose.pose.position.y = msg->position.y;
+	currentPose.pose.orientation = msg->orientation;
 }
 
 void odomCallback(const nav_msgs::Odometry &msg)
@@ -55,6 +79,8 @@ int main(int argc,char* argv[])
 	odomSub = nh->subscribe("odom", 1, odomCallback);
 	cmdSub = nh->subscribe("cmd_vel", 1, cmdCallback);
 	motSub = nh->subscribe("motor_status", 1, motorCallback);
+	poseSub = nh->subscribe("robot_pose", 1, poseCallback);
+        bumperPose = nh->advertise<geometry_msgs::PoseStamped>("/virtual_bumper", 1);
 
 	ros::spin();
 	return 0;
